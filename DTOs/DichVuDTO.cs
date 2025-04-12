387732097ddd79cd8a3d8563ce7841cf001c1c16
torikkyun <nduc42176@gmail.com>
2025-04-12 20:40:43 +0700

    Xóa dto dịch vụ

--- DTOs/DichVuDTO.cs
+++ DTOs/DichVuDTO.cs
@@ -2,9 +2,5 @@ namespace QLKS_115_Nhom3_BE.DTOs
 {
     public class DichVuDTO
     {
-        public int MaDichVu { get; set; }
-        public string TenDichVu { get; set; } = null!;
-        public enum LoaiDichVu { get, set }
-        public string? GhiChu { get; set; }
     }
 }
